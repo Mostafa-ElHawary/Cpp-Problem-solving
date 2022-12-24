1777cc5e21c8dc13d64a18e7c43727d0548a20b8
Azzi-MO <alhwarymostafa@gmail.com>
2022-12-24 03:14:08 -0800

    06_homework_01_answer

---
+++ CPP for beginners/06_homework_01_answer.cpp
@@ -0,0 +1,47 @@
+
+// HomeWork 1 
+// > Given a anumber . Using only %2 how to know if the number is even or odd 
+// > Given a anumber . Using only %10 how to know if the number is even or odd 
+// > Given a anumber . Using only /2 how to know if the number is even or odd 
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    
+    // > Given a anumber . Using only %2 how to know if the number is even or odd 
+   
+            // int num;
+            // cin >> num;
+
+            // if(num % 2 == 0 )
+            // {
+            //     cout << num << "is even";
+            // }
+            // else
+            // {
+            //     cout << num << "is odd";
+            // }
+            // return 0;
+
+    // > Given a anumber . Using only %10 how to know if the number is even or odd 
+
+            // int num ;
+            // cin >> num ;
+
+            // if( num % 10 == 0 || num % 10 == 2 || num % 10 == 4 || num % 10 == 8  )
+            // {
+            //     cout << num << " is even";
+            // }
+            //     else
+            // {
+            //     cout << num << " is odd";
+            // }
+            // return 0;
+
+    // > Given a anumber . Using only /2 how to know if the number is even or odd 
+
+}
