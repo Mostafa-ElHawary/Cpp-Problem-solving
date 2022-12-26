d7f277f7c99d740cc776981a1a3153b9c77975f8
Azzi-Mo <alhwarymostafa@gmail.com>
2022-12-26 17:37:02 +0200

    06_homework_04_answer

--- CPP for beginners/06_homework_03_answer.cpp
+++ CPP for beginners/06_homework_03_answer.cpp
@@ -18,13 +18,15 @@ int n;
 	int last1 = n % 10;
 	n /= 10;
 
-	int last2 = n % 10;
-	n /= 10;
+    cout << n;
+
+	// int last2 = n % 10;
+	// n /= 10;
 
 
-	int last3 = n % 10;
-	n /= 10;
+	// int last3 = n % 10;
+	// n /= 10;
 
-	cout << last1 + last2 + last3 << "\n";
+	// cout << last1 + last2 + last3 << "\n";
     return 0;
 }
---
+++ CPP for beginners/06_homework_04_answer.cpp
@@ -0,0 +1,17 @@
+//write a program that reads an integer and print the 4th from the right side if no such digit , print 0
+//input
+//15 => 0
+//125 => 0
+//1000 => 1
+//5001 => 5
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{  int num;
+    cin >> num;
+    cout << (num / 1000) % 10 << "\n";
+    return 0;
+}
